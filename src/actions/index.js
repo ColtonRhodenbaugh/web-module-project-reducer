b871fb86b4e042551c183703f43d6e1bd059608e
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-MacBook-Air.local>
2023-03-28 17:47:05 -0500

    all done

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_VALUE ='MEMORY_VALUE';
+export const MEMORY_ADD = 'MEMORY_ADD'
+export const MEMORY_RESET = 'MEMORY_RESET'
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,20 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+export const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload:operator});
+}
+
+export const clearDisplay = () => {
+    return ({type:CLEAR_DISPLAY})
+}
+export const memoryValue = () => {
+    return ({type:MEMORY_VALUE})
+}
+export const memoryAdd = () => {
+    return ({type:MEMORY_ADD})
+}
+export const memoryReset = () => {
+    return ({type:MEMORY_RESET})
 }
